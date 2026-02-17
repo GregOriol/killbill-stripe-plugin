bd772a8b99d22a42dfae9a2f2ca0c873a0384453
Greg ORIOL <greg@gregoriol.net>
2026-02-17 18:06:52 +0100

    Added off_session parameter for payments

--- src/main/java/org/killbill/billing/plugin/stripe/StripePaymentPluginApi.java
+++ src/main/java/org/killbill/billing/plugin/stripe/StripePaymentPluginApi.java
@@ -813,7 +813,7 @@ public class StripePaymentPluginApi extends PluginPaymentPluginApi<StripeRespons
                                                  paymentIntentParams.put("amount", KillBillMoney.toMinorUnits(currency.toString(), amount));
                                                  paymentIntentParams.put("currency", currency.toString());
                                                  paymentIntentParams.put("capture_method", captureMethod.value);
-                                                 // TODO Do we need to switch to manual confirmation to be able to set off_session=recurring?
+                                                 paymentIntentParams.put("off_session", true);
                                                  paymentIntentParams.put("confirm", true);
                                                  // See https://stripe.com/docs/api/payment_intents/create#create_payment_intent-return_url
                                                  final String returnUrl = PluginProperties.findPluginPropertyValue("return_url", properties);
